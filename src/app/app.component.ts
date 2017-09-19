67ac2f3657c8fc31a44058e44be27811842b51c4
Mahendraa04 <mahendrapandey756@gmail.com>
2017-09-19 11:54:48 -0700

    Mdal Driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormControl  } from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  userForm=new FormGroup({
+    name:new FormControl(),
+    // email:new FormControl(),
+    // address:new FormGroup({
+    //   street:new FormControl(),
+    //   city:new FormControl(),
+    //   postalCode:new FormControl()
+    // })
+  })
+
+  onSubmit()
+  {
+    console.log(this.userForm.value);
+  }
 }
